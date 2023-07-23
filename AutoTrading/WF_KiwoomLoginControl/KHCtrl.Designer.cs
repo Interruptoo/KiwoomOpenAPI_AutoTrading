7be274e02c1451388beacdad34a20fc5cb5e2dcc
yuminho <dbalsgh1312@naver.com>
2023-07-23 11:36:37 +0900

    자동매매 프로그램 키움 OpenAPI 참조추가 및 로그인 Control 추가 로그인컨트롤은 Winform으로 구성했고 WPF 메인 프로젝트에서 Winform 사용자정의 컨트롤 불러와서 사용

---
+++ AutoTrading/WF_KiwoomLoginControl/KHCtrl.Designer.cs
@@ -0,0 +1,65 @@
+﻿
+namespace WF_KiwoomLoginControl
+{
+    partial class KHCtrl
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(KHCtrl));
+            this.axKHOpenAPI = new AxKHOpenAPILib.AxKHOpenAPI();
+            ((System.ComponentModel.ISupportInitialize)(this.axKHOpenAPI)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // axKHOpenAPI
+            // 
+            this.axKHOpenAPI.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.axKHOpenAPI.Enabled = true;
+            this.axKHOpenAPI.Location = new System.Drawing.Point(0, 0);
+            this.axKHOpenAPI.Name = "axKHOpenAPI";
+            this.axKHOpenAPI.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axKHOpenAPI.OcxState")));
+            this.axKHOpenAPI.Size = new System.Drawing.Size(800, 450);
+            this.axKHOpenAPI.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.axKHOpenAPI);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.axKHOpenAPI)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private AxKHOpenAPILib.AxKHOpenAPI axKHOpenAPI;
+    }
+}
+
---
+++ AutoTrading/WF_KiwoomLoginControl/KHCtrl.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WF_KiwoomLoginControl
+{
+    public partial class KHCtrl : UserControl
+    {
+        public KHCtrl()
+        {
+            InitializeComponent();
+        }
+
+        public void Login()
+        {
+            axKHOpenAPI.CommConnect();
+        }
+    }
+}
